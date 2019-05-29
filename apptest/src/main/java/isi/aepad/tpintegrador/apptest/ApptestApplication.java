72dfa4752ceabe643c200a48ead26a0362cb03fa
Pedro Capdevila <pedro.capdevila@hotmail.com>
2019-05-29 18:44:50 -0300

    Ruta corregida

--- apptest/src/main/java/isi/aepad/tpintegrador/apptest/ApptestApplication.java
+++ apptest/src/main/java/isi/aepad/tpintegrador/apptest/ApptestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("isi.aepad.tpintegrador.ACDZ.repo")
+@EnableJpaRepositories("isi.aepad.tpintegrador.repository")
 public class ApptestApplication {
 
 	public static void main(String[] args) {
--- apptest/src/test/java/isi/aepad/tpintegrador/apptest/ApptestApplicationTests.java
+++ apptest/src/test/java/isi/aepad/tpintegrador/apptest/ApptestApplicationTests.java
@@ -1,4 +1,4 @@
-package isi.aepad.tpintegrador.ACDZ.apptest;
+package isi.aepad.tpintegrador.apptest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
